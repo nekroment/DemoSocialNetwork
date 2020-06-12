c3771b98f3edb2b3373633af4527e143253a3972
nekroment <adamcevs@gmail.com>
2020-06-12 10:08:47 +0300

    Updates

---
+++ precache-manifest.738059ad9dd1180bf57eefdf1640e8c1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2da6ded6a2b4e319f56da8cc6cba31",
+    "url": "/DemoSocialNetwork/index.html"
+  },
+  {
+    "revision": "da6c458c2ce67d82be2d",
+    "url": "/DemoSocialNetwork/static/css/3.7b7887e7.chunk.css"
+  },
+  {
+    "revision": "27b12f57d3008ee89cd9",
+    "url": "/DemoSocialNetwork/static/css/main.4d048dbd.chunk.css"
+  },
+  {
+    "revision": "b6ad0b607741a66c335b",
+    "url": "/DemoSocialNetwork/static/js/2.86d4b1fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DemoSocialNetwork/static/js/2.86d4b1fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da6c458c2ce67d82be2d",
+    "url": "/DemoSocialNetwork/static/js/3.103ca24a.chunk.js"
+  },
+  {
+    "revision": "27b12f57d3008ee89cd9",
+    "url": "/DemoSocialNetwork/static/js/main.9c3aaef8.chunk.js"
+  },
+  {
+    "revision": "dd7f22c8373fffd213f6",
+    "url": "/DemoSocialNetwork/static/js/runtime-main.b0121e07.js"
+  },
+  {
+    "revision": "bd8f56164ae2624fb25444e9a9c092ec",
+    "url": "/DemoSocialNetwork/static/media/Loader.bd8f5616.svg"
+  }
+]);
