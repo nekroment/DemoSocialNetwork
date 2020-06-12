f1c54218cbbaf621784c61657a2efad6a79606be
nekroment <adamcevs@gmail.com>
2020-06-12 10:29:26 +0300

    no message

--- src/App.js
+++ src/App.js
@@ -19,9 +19,7 @@ class App extends React.Component {
     this.props.initializeApp()
   }
   render() {
-    if (!this.props.initialized) {
-      return <Preloader />
-    }
+    
     return (
       <div className='app-wrapper'>
         <HeaderContainer />
